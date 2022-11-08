924cb8e0cc7ca6a0cdb8b977ca7198c279d0d4d0
brianrecoba <brian12fernandes@gmail.com>
2022-11-08 11:14:23 -0300

    Aula 01 completa

---
+++ pages/index.js
@@ -0,0 +1,85 @@
+import config from "../config.json";
+import styled from "styled-components";
+import { CSSReset } from "../src/components/CSSReset";
+import Menu from "../src/components/Menu"
+import {StyledTimeline} from "../src/components/Timeline"
+
+function HomePage() {
+  console.log(config.playlists);
+
+  return (
+    <>
+      <CSSReset />
+      <div>
+        <Menu />
+        <Header />
+        <Timeline playlists={config.playlists} />
+      </div>
+    </>
+  );
+}
+
+export default HomePage;
+
+
+
+const StyleHeader = styled.div`
+  img {
+    width: 80px;
+    height: 80px;
+    border-radius: 50%;
+  }
+  .user-info {
+    margin-top: 50px;
+    display: flex;
+    align-items: center;
+    width: 100%;
+    padding: 16px 32px;
+    gap: 16px;
+  }
+`;
+function Header() {
+  return (
+    <StyleHeader>
+      <section className="user-info">
+        {/* <img src="" alt="" srcset="" /> */}
+        <img
+          src={`https://github.com/${config.github}.png`}
+          alt={`Foto do ${config.github}`}
+          srcset=""
+        />
+        <div>
+          <h2>{config.name}</h2>
+          <p>{config.job}</p>
+        </div>
+      </section>
+    </StyleHeader>
+  );
+}
+
+function Timeline(props) {
+  const playlistNames = Object.keys(props.playlists);
+  return (
+    <StyledTimeline>
+      {playlistNames.map((playlistNames) => {
+        const videos = props.playlists[playlistNames];
+
+        return (
+          <section>
+            <h2>{playlistNames}</h2>
+            <div>
+              {videos.map((video) => {
+                return (
+                  <a href={video.url}>
+                    <img src={video.thumb} alt="" />
+                    <span>{video.title}</span>
+                  </a>
+                );
+              })}
+            </div>
+          </section>
+        );
+      })}
+    </StyledTimeline>
+  );
+}

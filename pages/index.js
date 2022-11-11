e7f2a4dd18f8b2eeb5a738ad0ed090c6b28ece87
Brian Fernandes <brian12fernandes@gmail.com>
2022-11-11 16:55:43 -0300

    Aula 03 incompleta

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import config from "../config.json";
 import styled from "styled-components";
-import { CSSReset } from "../src/components/CSSReset";
 import Menu from "../src/components/Menu"
 import {StyledTimeline} from "../src/components/Timeline"
 
@@ -10,7 +9,6 @@ function HomePage() {
 
   return (
     <>
-      <CSSReset />
       <div>
         <Menu valorDoFiltro={valorDoFiltro} setValorDoFiltro={setValorDoFiltro}/>
         <Header />
@@ -25,6 +23,8 @@ export default HomePage;
 
 
 const StyleHeader = styled.div`
+  background-color: ${( {theme} ) => theme.backgroundLevel1};
+
   img {
     width: 80px;
     height: 80px;
